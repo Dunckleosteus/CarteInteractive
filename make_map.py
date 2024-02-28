cec0d88ce42876e31320ccdf454810617832c969
George Husband <ghus266@gmail.com>
2024-02-28 16:39:22 +0100

    adrien's code added, updated map

--- make_map.py
+++ make_map.py
@@ -1,11 +1,104 @@
+# Handling imports
+from geopy.geocoders import Nominatim
 import folium
+import pandas as pd
+import numpy as np
+import geopandas as gpd
+import os
 
-# Create a map object
-m = folium.Map(location=[51.5074, -0.1278], zoom_start=10)
 
-# Add markers or other features to the map
-folium.Marker(location=[51.5074, -0.1278], popup='London').add_to(m)
-folium.Marker(location=[48.8566, 2.3522], popup='Paris').add_to(m)
+# importing data 
+path = os.path.join("data.xlsx")
+dataset = pd.read_excel(path)
+# renaming columns 
+ancien_nom_vers_nouveau_nom = {
+    'Nom': 'Identite',
+    'Dans quelle entreprise es-tu en stage ?': 'Entreprise1',
+    'Dans quelle ville a lieu ton stage ?': 'Ville','Quelle est ton entreprise ?':'Entreprise2','Dans quelle ville es-tu en alternance ?':'Ville2','Code postal de ton lieu de stage':'ZIP','Raconte nous ce que tu fais ou tes missions dans ce petit paragraphe suivant (pas obligatoire)':'Missions'
+    # ...
+}
+dataset2 = dataset.rename(columns=ancien_nom_vers_nouveau_nom)
+head1 = ['Identite','Entreprise1','Ville','Entreprise2','Ville2','ZIP','Missions']
 
-# Save the map as an HTML file
+# not sure why we are making a subset
+df = dataset2[head1]
+# Adding columns for x, y coordinates
+cordX = pd.Series([0] * len(df))
+cordY = pd.Series([0] * len(df))
+
+
+
+# adding text columns
+Ville0 = pd.Series(['NULL'] * len(df))
+Entreprise0 = pd.Series(['NULL'] * len(df))
+
+df['Ville0'] = Ville0
+df['Entreprise0'] = Entreprise0
+
+df['X'] = cordX
+df['Y'] = cordY
+
+
+#remplacer dans les nouvelles colonnes les valeurs non nulles
+df['Ville0'] = df['Ville'].fillna(df['Ville2'])
+df['Entreprise0'] = df['Entreprise1'].fillna(df['Entreprise2'])
+# Vérifiez les valeurs de V0
+
+# saving to csv (not sure why)
+df.to_csv('CSV.csv')
+
+#Import des coordonnées X et Y
+def g(df):
+  # Vérifie si la valeur est un entier
+  is_numeric = df['ZIP'].str.isdigit()
+  # Vérifie si la valeur a 5 caractères
+  is_correct_length = df['ZIP'].str.len() == 5
+  # Filtre le dataframe en conservant uniquement les lignes valides
+  df = df[is_numeric & is_correct_length]
+  return df
+
+df = g(df.copy())
+
+# Affichez les premières lignes du DataFrame
+df
+locator = Nominatim(user_agent='my_app')
+df['Adresse'] = df['ZIP'].apply(lambda x: str(x) + ', France')
+
+df['X'] = df['Adresse'].apply(lambda x: locator.geocode(x).latitude)
+df['Y'] = df['Adresse'].apply(lambda x: locator.geocode(x).longitude)
+
+colonnes_a_conserver2 = ['Identite', 'Entreprise0', 'Ville0', 'X', 'Y', 'Missions']
+full_df = df[colonnes_a_conserver2]
+
+full_df.to_csv('Clean.csv')
+
+datasetmap=pd.read_csv(os.path.join("Clean.csv"))
+
+gens = gpd.GeoDataFrame(datasetmap,
+    geometry = gpd.points_from_xy(datasetmap['Y'], datasetmap['X']),
+    crs = 'EPSG:4326').to_file('gens_point.shp')
+
+
+gens_point = gpd.read_file(os.path.join("gens_point.shp"))
+gens_point = gens_point.to_crs(4326)
+
+attribution = "Coeur sur vous les dodos"
+
+m = gens_point.explore(
+    location=[48.858053, 2.2944991],
+    tiles="cartodb positron",
+    zoom_start=6,
+    width=1000,
+    height=500,
+    attr=attribution,
+)
+
+
+folium.Marker(
+    location=[49.460715173019175, 2.0705808327370465],
+    popup='Maison',
+    icon=folium.Icon(color='green', icon='ok-sign'),
+).add_to(m)
+
+
 m.save('map.html')
